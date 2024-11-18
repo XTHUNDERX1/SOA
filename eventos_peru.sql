95f4e4f73fff656bcbb3c6ffbfecbf635b8a164d
williamIT207 <william220418@gmail.com>
2024-11-18 01:11:41 -0500

    update bd

--- eventos_peru.sql
+++ eventos_peru.sql
@@ -1,69 +1,238 @@
--- Base de datos: eventos_peru
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 18-11-2024 a las 01:59:55
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
 
--- Estructura de tabla para la tabla clientes
-CREATE TABLE clientes (
-  ClienteID SERIAL PRIMARY KEY,
-  NombreCliente VARCHAR(100) NOT NULL,
-  Telefono VARCHAR(15),
-  Email VARCHAR(100),
-  Direccion VARCHAR(255)
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
--- Estructura de tabla para la tabla eventos
-CREATE TABLE eventos (
-  EventoID SERIAL PRIMARY KEY,
-  TipoEvento VARCHAR(50) NOT NULL,
-  Fecha DATE NOT NULL,
-  Lugar VARCHAR(255) NOT NULL,
-  ClienteID INT,
-  ProveedorID INT,
-  FOREIGN KEY (ClienteID) REFERENCES clientes (ClienteID),
-  FOREIGN KEY (ProveedorID) REFERENCES proveedores (ProveedorID)
-);
 
--- Estructura de tabla para la tabla feedback
-CREATE TABLE feedback (
-  FeedbackID SERIAL PRIMARY KEY,
-  EventoID INT,
-  Comentarios TEXT,
-  Calificacion INT CHECK (Calificacion >= 1 AND Calificacion <= 5),
-  FOREIGN KEY (EventoID) REFERENCES eventos (EventoID)
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
--- Estructura de tabla para la tabla proveedores
-CREATE TABLE proveedores (
-  ProveedorID SERIAL PRIMARY KEY,
-  NombreProveedor VARCHAR(100) NOT NULL,
-  Telefono VARCHAR(15),
-  Email VARCHAR(100),
-  Direccion VARCHAR(255),
-  Reputacion INT DEFAULT 0
-);
+--
+-- Base de datos: `eventos_peru`
+--
 
--- Estructura de tabla para la tabla roles
-CREATE TABLE roles (
-  RolID SERIAL PRIMARY KEY,
-  NombreRol VARCHAR(50) NOT NULL UNIQUE
-);
+-- --------------------------------------------------------
 
--- Volcado de datos para la tabla roles
-INSERT INTO roles (RolID, NombreRol) VALUES
+--
+-- Estructura de tabla para la tabla `clientes`
+--
+
+CREATE TABLE `clientes` (
+  `ClienteID` int(11) NOT NULL,
+  `NombreCliente` varchar(100) NOT NULL,
+  `Telefono` varchar(15) DEFAULT NULL,
+  `Email` varchar(100) DEFAULT NULL,
+  `Direccion` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `eventos`
+--
+
+CREATE TABLE `eventos` (
+  `EventoID` int(11) NOT NULL,
+  `TipoEvento` varchar(50) NOT NULL,
+  `Fecha` date NOT NULL,
+  `Lugar` varchar(255) NOT NULL,
+  `ClienteID` int(11) DEFAULT NULL,
+  `ProveedorID` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `feedback`
+--
+
+CREATE TABLE `feedback` (
+  `FeedbackID` int(11) NOT NULL,
+  `EventoID` int(11) DEFAULT NULL,
+  `Comentarios` text DEFAULT NULL,
+  `Calificacion` int(11) DEFAULT NULL CHECK (`Calificacion` >= 1 and `Calificacion` <= 5)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `proveedores`
+--
+
+CREATE TABLE `proveedores` (
+  `ProveedorID` int(11) NOT NULL,
+  `NombreProveedor` varchar(100) NOT NULL,
+  `Telefono` varchar(15) DEFAULT NULL,
+  `Email` varchar(100) DEFAULT NULL,
+  `Direccion` varchar(255) DEFAULT NULL,
+  `Reputacion` int(11) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `roles`
+--
+
+CREATE TABLE `roles` (
+  `RolID` int(11) NOT NULL,
+  `NombreRol` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `roles`
+--
+
+INSERT INTO `roles` (`RolID`, `NombreRol`) VALUES
 (1, 'Administrador'),
-(2, 'Moderador'),
-(3, 'Usuario');
+(3, 'Moderador'),
+(2, 'Usuario');
 
--- Estructura de tabla para la tabla usuarios
-CREATE TABLE usuarios (
-  UsuarioID SERIAL PRIMARY KEY,
-  NombreUsuario VARCHAR(50) NOT NULL,
-  Email VARCHAR(100) NOT NULL UNIQUE,
-  Contraseña VARCHAR(255) NOT NULL,
-  RolID INT,
-  FOREIGN KEY (RolID) REFERENCES roles (RolID)
-);
+-- --------------------------------------------------------
 
--- Volcado de datos para la tabla usuarios
-INSERT INTO usuarios (UsuarioID, NombreUsuario, Email, Contraseña, RolID) VALUES
-(1, 'admin', 'test@gmail.com', '$2y$10$HeEVDqxUERsrBzmHi/eBae/78DsfRYvKrRFgNQf8Um5rtXqY0X29G', 1),
-(2, 'william', 'william@gmail.com', '$2y$10$j2zk4CBmN/RcGwjK5GmobO.Pjo18NPu9iCLT9rv0dKvTVlB.NaRV.', 2),
-(3, 'admin2', 'admin2@gmail.com', '$2y$10$ggtlYttW783fIXx3BXU6vO09AKV3Ew87U29H9aU0.TmtEq9p6sPrm', 1);
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `UsuarioID` int(11) NOT NULL,
+  `NombreUsuario` varchar(50) NOT NULL,
+  `Email` varchar(100) NOT NULL,
+  `Contraseña` varchar(255) NOT NULL,
+  `RolID` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`UsuarioID`, `NombreUsuario`, `Email`, `Contraseña`, `RolID`) VALUES
+(3, 'admin', 'test@gmail.com', '$2y$10$HeEVDqxUERsrBzmHi/eBae/78DsfRYvKrRFgNQf8Um5rtXqY0X29G', 1),
+(4, 'william', 'william@gmail.com', '$2y$10$j2zk4CBmN/RcGwjK5GmobO.Pjo18NPu9iCLT9rv0dKvTVlB.NaRV.', 2),
+(5, 'admin2', 'admin2@gmail.com', '$2y$10$ggtlYttW783fIXx3BXU6vO09AKV3Ew87U29H9aU0.TmtEq9p6sPrm', 1);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `clientes`
+--
+ALTER TABLE `clientes`
+  ADD PRIMARY KEY (`ClienteID`),
+  ADD UNIQUE KEY `Email` (`Email`);
+
+--
+-- Indices de la tabla `eventos`
+--
+ALTER TABLE `eventos`
+  ADD PRIMARY KEY (`EventoID`),
+  ADD KEY `ClienteID` (`ClienteID`),
+  ADD KEY `ProveedorID` (`ProveedorID`);
+
+--
+-- Indices de la tabla `feedback`
+--
+ALTER TABLE `feedback`
+  ADD PRIMARY KEY (`FeedbackID`),
+  ADD KEY `EventoID` (`EventoID`);
+
+--
+-- Indices de la tabla `proveedores`
+--
+ALTER TABLE `proveedores`
+  ADD PRIMARY KEY (`ProveedorID`),
+  ADD UNIQUE KEY `Email` (`Email`);
+
+--
+-- Indices de la tabla `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`RolID`),
+  ADD UNIQUE KEY `NombreRol` (`NombreRol`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`UsuarioID`),
+  ADD UNIQUE KEY `Email` (`Email`),
+  ADD KEY `RolID` (`RolID`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `clientes`
+--
+ALTER TABLE `clientes`
+  MODIFY `ClienteID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `eventos`
+--
+ALTER TABLE `eventos`
+  MODIFY `EventoID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `feedback`
+--
+ALTER TABLE `feedback`
+  MODIFY `FeedbackID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `proveedores`
+--
+ALTER TABLE `proveedores`
+  MODIFY `ProveedorID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `RolID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `UsuarioID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `eventos`
+--
+ALTER TABLE `eventos`
+  ADD CONSTRAINT `eventos_ibfk_1` FOREIGN KEY (`ClienteID`) REFERENCES `clientes` (`ClienteID`),
+  ADD CONSTRAINT `eventos_ibfk_2` FOREIGN KEY (`ProveedorID`) REFERENCES `proveedores` (`ProveedorID`);
+
+--
+-- Filtros para la tabla `feedback`
+--
+ALTER TABLE `feedback`
+  ADD CONSTRAINT `feedback_ibfk_1` FOREIGN KEY (`EventoID`) REFERENCES `eventos` (`EventoID`);
+
+--
+-- Filtros para la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD CONSTRAINT `usuarios_ibfk_1` FOREIGN KEY (`RolID`) REFERENCES `roles` (`RolID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
