6dc7ab6f1b947f7e214eb822a7c8ecded9c8aa40
William <william.marengo@itservice.com.pe>
2024-11-19 18:26:34 -0500

    redireccionamiento

--- agregar_proveedores.php
+++ agregar_proveedores.php
@@ -11,7 +11,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $sql = "INSERT INTO proveedores (NombreProveedor, Telefono, Email, Direccion, Reputacion) 
             VALUES ('$nombre', '$telefono', '$email', '$direccion', $reputacion)";
     if ($conn->query($sql)) {
-        header('Location:login.html ');
+        header('Location:dashboard_proveedores.php ');
     } else {
         echo 'Error: ' . $conn->error;
     }
